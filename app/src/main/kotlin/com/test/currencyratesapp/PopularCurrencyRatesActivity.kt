2312a8f2ce3e2543a6dc0b3d39527eebea1e783b
Aleksei <alekseyka2000@gmail.com>
2022-06-14 21:23:13 +0300

    created app

---
+++ app/src/main/kotlin/com/test/currencyratesapp/PopularCurrencyRatesActivity.kt
@@ -0,0 +1,11 @@
+package com.test.currencyratesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopularCurrencyRatesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
